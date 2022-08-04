fd177d4193b6d97838732da459860cef0a2bf7ab
Ox5un5h1n3 <5un5h1n34096@protonmail.com>
2022-08-04 13:38:34 +0530

    init

---
+++ app/src/main/java/com/ox5un5h1n3/zulo/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ox5un5h1n3.zulo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
