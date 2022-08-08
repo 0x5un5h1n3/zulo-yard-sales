2432a40b37147d1793a3f9b9ce65a70548c528a8
Ox5un5h1n3 <5un5h1n34096@protonmail.com>
2022-08-08 22:07:13 +0530

    Fix Product Detail bug, Add Contact Seller

--- app/src/main/java/com/ox5un5h1n3/zulo/ui/search/AllProductAdapter.java
+++ app/src/main/java/com/ox5un5h1n3/zulo/ui/search/AllProductAdapter.java
@@ -2,35 +2,54 @@ package com.ox5un5h1n3.zulo.ui.search;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.button.MaterialButton;
 import com.google.android.material.card.MaterialCardView;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.google.android.material.textview.MaterialTextView;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.ox5un5h1n3.zulo.R;
 import com.ox5un5h1n3.zulo.data.model.Product;
+import com.ox5un5h1n3.zulo.data.model.UserDetail;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 public class AllProductAdapter extends RecyclerView.Adapter<AllProductAdapter.MyAllProductViewHolder> {
 
-    private List<Product> mProductList;
+    public static List<Product> mProductList;
+    public static int getPosition;
     private Activity mActivity;
 
     private MaterialTextView mProductName;
     private MaterialTextView mProductPrice;
     private MaterialTextView mProductDescription;
     private ImageView mProductImage;
+
+    private MaterialTextView mSellerName;
+    private MaterialTextView mSellerAddress;
+    private MaterialTextView mSellerMo;
+
     Product product;
 
     // 1st called
@@ -56,11 +75,15 @@ public class AllProductAdapter extends RecyclerView.Adapter<AllProductAdapter.My
     //5th called
     @SuppressLint("SetTextI18n")
     @Override
-    public void onBindViewHolder(@NonNull @NotNull MyAllProductViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull @NotNull MyAllProductViewHolder holder, @SuppressLint("RecyclerView") int position) {
         Product product = mProductList.get(position);
         holder.mTvProductName.setText(product.getProductName());
         holder.mTvProductDesc.setText(product.getProductDescription());
         holder.mTvProductPrice.setText("$ " + product.getProductPrice());
+
+
+
+
 
         //for image - using Glide
 //        Glide.with(holder.itemView).load(product.getProductImage()).into(holder.mImageView);
@@ -89,6 +112,8 @@ public class AllProductAdapter extends RecyclerView.Adapter<AllProductAdapter.My
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                getPosition = position;
 
 
 //                Button viewAll = view.findViewById(R.id.btnAddYardSale);
@@ -138,7 +163,7 @@ public class AllProductAdapter extends RecyclerView.Adapter<AllProductAdapter.My
 
 
 
-//                        Navigation.findNavController(view).navigate(R.id.action_navigation_search_main_to_search_item_details);
+                        Navigation.findNavController(view).navigate(R.id.action_navigation_search_main_to_search_item_details);
 
 
 
@@ -147,34 +172,39 @@ public class AllProductAdapter extends RecyclerView.Adapter<AllProductAdapter.My
     }
 
 
-//    private class ViewMaterialDialog {
-//
-//        EditText userName;
-//        EditText userPhoneNo;
-//        private ProgressDialog mDialog;
-//
-//        public void showDialog(Activity activity){
-//            MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(activity);
-//            LayoutInflater inflater = activity.getLayoutInflater();
-//            View dialogView = inflater.inflate(R.layout.add_user_info, null);
-//            builder.setView(dialogView);
-//
-//            mDialog = new ProgressDialog(activity);
-//            mDialog.setMessage("Reserving product");
-//            mDialog.setCancelable(false);
-//
+    private class ViewMaterialDialog {
+
+        EditText userName, userPhoneNo;
+        MaterialTextView sellerName, SellerPhoneNo, SellerLocation;
+        private ProgressDialog mDialog;
+
+        public void showDialog(Activity activity){
+            MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(activity);
+            LayoutInflater inflater = activity.getLayoutInflater();
+            View dialogView = inflater.inflate(R.layout.add_user_info, null);
+            builder.setView(dialogView);
+
+            mDialog = new ProgressDialog(activity);
+            mDialog.setMessage("Reserving product");
+            mDialog.setCancelable(false);
+
 //            userName = dialogView.findViewById(R.id.et_name);
 //            userPhoneNo = dialogView.findViewById(R.id.et_phone_no);
-//
-//            MaterialButton dialogBtn_cancel = dialogView.findViewById(R.id.btn_request_reserve);
-//            dialogBtn_cancel.setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View v) {
-//                    mDialog.show();
-//
+
+
+            mSellerName = dialogView.findViewById(R.id.tv_seller_name);
+            mSellerAddress = dialogView.findViewById(R.id.tv_seller_address);
+            mSellerMo = dialogView.findViewById(R.id.tv_seller_mobile);
+
+            MaterialButton dialogBtn_cancel = dialogView.findViewById(R.id.btn_request_reserve);
+            dialogBtn_cancel.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mDialog.show();
+
 //                    String name = userName.getText().toString().trim();
 //                    String phone = userPhoneNo.getText().toString().trim();
-//
+
 //                    if (name.isEmpty()){
 //                        Toast.makeText(activity.getApplicationContext(), "Name is empty", Toast.LENGTH_SHORT).show();
 //                        mDialog.cancel();
@@ -190,22 +220,24 @@ public class AllProductAdapter extends RecyclerView.Adapter<AllProductAdapter.My
 //                        mDialog.cancel();
 //                        return;
 //                    }
-//
-//                    checkIsProductReservedAndUpdate(name, phone);
-//                }
-//            });
-//
-//            AlertDialog alertDialog = builder.create();
-//            alertDialog.show();
-//        }
-//
-//
-//
-//        private void checkIsProductReservedAndUpdate(String userName, String userPhoneNo) {
+
+                    checkIsProductReservedAndUpdate();
+                }
+            });
+
+            AlertDialog alertDialog = builder.create();
+            alertDialog.show();
+//            getSellerInfo();
+        }
+
+
+
+//        private void checkIsProductReservedAndUpdate1(Activity activity, String userName, String userPhoneNo) {
 //            FirebaseFirestore.getInstance().collection("Products").document(product.getProductKey()).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
 //                @Override
 //                public void onSuccess(DocumentSnapshot documentSnapshot) {
-//
+//                    Toast.makeText(mActivity, "Test", Toast.LENGTH_SHORT).show();
+////
 //                }
 ////                @Override
 ////                public void onSuccess(DocumentSnapshot documentSnapshot) {
@@ -238,8 +270,61 @@ public class AllProductAdapter extends RecyclerView.Adapter<AllProductAdapter.My
 ////                }
 //            });
 //        }
-//
-//    }
+
+        private void getSellerInfo(){
+        FirebaseFirestore.getInstance().collection("Users").document(product.getProductOwnerUid()).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                UserDetail userDetail = documentSnapshot.toObject(UserDetail.class);
+                if (userDetail != null){
+                    mSellerName.setText("Seller Name : " + userDetail.getUsername());
+                    mSellerAddress.setText("Seller address : " + userDetail.getAddress());
+                    mSellerMo.setText("Seller phone no. : " + userDetail.getPhoneNumber());
+                } else {
+                    Toast.makeText(mActivity.getApplicationContext(), "Seller not found", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        }
+
+        private void checkIsProductReservedAndUpdate() {
+            FirebaseFirestore.getInstance().collection("Products").document(product.getProductKey()).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                @Override
+                public void onSuccess(DocumentSnapshot documentSnapshot) {
+                    Product product = documentSnapshot.toObject(Product.class);
+                    UserDetail userDetail = documentSnapshot.toObject(UserDetail.class);
+                    if (product != null){
+                        if (!product.getProductReserve()){
+                            Map<String, Object> updateReserveProduct = new HashMap<>();
+                            updateReserveProduct.put("productReserve", true);
+                            updateReserveProduct.put("customerName", userDetail.getUsername());
+                            updateReserveProduct.put("customerPhoneNo", userDetail.getPhoneNumber());
+
+                            FirebaseFirestore.getInstance().collection("Products").document(product.getProductKey()).update(updateReserveProduct).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void unused) {
+                                    mDialog.cancel();
+//                                    activity.finish();
+                                    Toast.makeText(mActivity.getApplicationContext(), "Reserve request sent successfully", Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                        }
+                        else {
+                            Toast.makeText(mActivity.getApplicationContext(), "Someone already requested for this product", Toast.LENGTH_LONG).show();
+//                            activity.finish();
+                        }
+                    } else {
+                        Toast.makeText(mActivity.getApplicationContext(), "This product might be removed", Toast.LENGTH_LONG).show();
+//                        activity.finish();
+                    }
+                    mDialog.dismiss();
+                }
+            });
+        }
+
+
+    }
 
     // 2nd called
     @Override
@@ -255,6 +340,9 @@ public class AllProductAdapter extends RecyclerView.Adapter<AllProductAdapter.My
         private final TextView mTvProductPrice;
         private final ImageView mImageView;
         private final MaterialCardView cvProduct;
+
+
+
 
         public MyAllProductViewHolder(@NonNull @NotNull View itemView) {
             super(itemView);
--- app/src/main/java/com/ox5un5h1n3/zulo/ui/search/SearchProductDetail.java
+++ app/src/main/java/com/ox5un5h1n3/zulo/ui/search/SearchProductDetail.java
@@ -1,8 +1,12 @@
 package com.ox5un5h1n3.zulo.ui.search;
 
+import static com.ox5un5h1n3.zulo.ui.search.AllProductAdapter.getPosition;
+import static com.ox5un5h1n3.zulo.ui.search.AllProductAdapter.mProductList;
+
 import android.annotation.SuppressLint;
-import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,17 +17,17 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
+import com.bumptech.glide.Glide;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.button.MaterialButton;
-import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.google.android.material.textview.MaterialTextView;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.ox5un5h1n3.zulo.R;
 import com.ox5un5h1n3.zulo.data.model.Product;
+import com.ox5un5h1n3.zulo.data.model.UserDetail;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -39,7 +43,12 @@ public class SearchProductDetail extends Fragment {
     private MaterialTextView mSellerName;
     private MaterialTextView mSellerAddress;
     private MaterialTextView mSellerMo;
-    private Product product;
+    private ProgressDialog mDialog;
+
+    private MaterialButton btnContactSeller;
+
+
+    private String sellerContactNo;
 
 
 
@@ -70,33 +79,50 @@ public class SearchProductDetail extends Fragment {
         mProductName = view.findViewById(R.id.tv_product_name_det);
         mProductPrice = view.findViewById(R.id.tv_product_price);
         mProductDescription = view.findViewById(R.id.tv_product_desc);
+
         mProductImage = view.findViewById(R.id.iv_product);
         mReserve = view.findViewById(R.id.btn_reserve);
         mSellerName = view.findViewById(R.id.tv_seller_name);
         mSellerAddress = view.findViewById(R.id.tv_seller_address);
         mSellerMo = view.findViewById(R.id.tv_seller_mobile);
 
-        getPreviousScreenProductData();
-        reserveButton();
+        btnContactSeller = view.findViewById(R.id.btn_contact_seller);
+
+        getProductData();
+        reserveProduct();
+        dialTheSeller();
 
     }
 
-    public void reserveButton() {
+    private void dialTheSeller() {
+        btnContactSeller.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:"+sellerContactNo));
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void reserveProduct() {
         mReserve.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ViewDialog viewDialog = new ViewDialog();
-                viewDialog.showDialog(getActivity());
+//                ViewDialog viewDialog = new ViewDialog();
+//                viewDialog.showDialog(getActivity());
+
+                checkIsProductReservedAndUpdate();
             }
         });
     }
 
 
     @SuppressLint("SetTextI18n")
-    private void getPreviousScreenProductData() {
+    private void getProductData() {
 //        product = (Product) getIntent().getSerializableExtra("Product");
 //        product = (Product) getActivity().getIntent().getSerializableExtra("Product");
-
+//
 //        product = (Product) getActivity().getIntent().getSerializableExtra("Product");
 //        Bundle bundle = this.getArguments();
 //        if (bundle != null) {
@@ -106,26 +132,74 @@ public class SearchProductDetail extends Fragment {
 //        product = (Product) getActivity().getIntent().getSerializableExtra("Product");
 //        product = (Product) getActivity().getIntent().getExtras().getSerializable("Product");
 
+//        int position = 0;
+        Product product = mProductList.get(getPosition);
+
 //        mProductName.setText("Hello");
-//        mProductName.setText(product.getProductName());
-//        mProductPrice.setText("Product Price : " + product.getProductPrice());
-//        mProductDescription.setText("Product description : " + product.getProductDescription());
-//        Glide.with(this).load(product.getProductImage()).into(mProductImage);
+//        Toast.makeText(getActivity(), product.getProductName(), Toast.LENGTH_SHORT).show();
+        mProductName.setText(product.getProductName());
+        mProductPrice.setText("Product Price : " + product.getProductPrice());
+        mProductDescription.setText("Product description : " + product.getProductDescription());
+        Glide.with(this).load(product.getProductImage()).into(mProductImage);
 
-//        FirebaseFirestore.getInstance().collection("Users").document(product.getProductOwnerUid()).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-//            @Override
-//            public void onSuccess(DocumentSnapshot documentSnapshot) {
-//                UserDetail userDetail = documentSnapshot.toObject(UserDetail.class);
-//                if (userDetail != null){
-//                    mSellerName.setText("Seller Name : " + userDetail.getUsername());
-//                    mSellerAddress.setText("Seller address : " + userDetail.getAddress());
-//                    mSellerMo.setText("Seller phone no. : " + userDetail.getPhoneNumber());
-//                } else {
-//                    Toast.makeText(getActivity(), "Seller not found", Toast.LENGTH_SHORT).show();
-//                }
-//            }
-//        });
+        FirebaseFirestore.getInstance().collection("Users").document(product.getProductOwnerUid()).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                UserDetail userDetail = documentSnapshot.toObject(UserDetail.class);
+                if (userDetail != null){
+                    mSellerName.setText("Seller Name : " + userDetail.getUsername());
+                    mSellerAddress.setText("Seller address : " + userDetail.getAddress());
+                    mSellerMo.setText("Seller phone no. : " + userDetail.getPhoneNumber());
+                    sellerContactNo = userDetail.getPhoneNumber();
+                } else {
+                    Toast.makeText(getActivity(), "Seller not found", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
+    }
+
+
+    private void checkIsProductReservedAndUpdate() {
+
+        Product product = mProductList.get(getPosition);
+
+        mDialog = new ProgressDialog(getActivity());
+        mDialog.setMessage("Reserving product");
+        mDialog.setCancelable(false);
+
+        FirebaseFirestore.getInstance().collection("Products").document(product.getProductKey()).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                Product product = documentSnapshot.toObject(Product.class);
+                UserDetail userDetail = documentSnapshot.toObject(UserDetail.class);
+                if (product != null){
+                    if (!product.getProductReserve()){
+                        Map<String, Object> updateReserveProduct = new HashMap<>();
+                        updateReserveProduct.put("productReserve", true);
+                        updateReserveProduct.put("customerName", userDetail.getUsername());
+                        updateReserveProduct.put("customerPhoneNo", userDetail.getPhoneNumber());
+
+                        FirebaseFirestore.getInstance().collection("Products").document(product.getProductKey()).update(updateReserveProduct).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void unused) {
+                                mDialog.cancel();
+//                                    activity.finish();
+                                Toast.makeText(getActivity(), "Reserve request sent successfully", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                    else {
+                        Toast.makeText(getActivity(), "Someone already requested for this product", Toast.LENGTH_LONG).show();
+//                            activity.finish();
+                    }
+                } else {
+                    Toast.makeText(getActivity(), "This product might be removed", Toast.LENGTH_LONG).show();
+//                        activity.finish();
+                }
+                mDialog.dismiss();
+            }
+        });
     }
 
     private class ViewDialog {
@@ -134,84 +208,5 @@ public class SearchProductDetail extends Fragment {
         EditText userPhoneNo;
         private ProgressDialog mDialog;
 
-        public void showDialog(Activity activity){
-            MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(activity);
-            LayoutInflater inflater = activity.getLayoutInflater();
-            View dialogView = inflater.inflate(R.layout.add_user_info, null);
-            builder.setView(dialogView);
-
-            mDialog = new ProgressDialog(activity);
-            mDialog.setMessage("Reserving product");
-            mDialog.setCancelable(false);
-
-            userName = dialogView.findViewById(R.id.et_name);
-            userPhoneNo = dialogView.findViewById(R.id.et_phone_no);
-
-            MaterialButton dialogBtn_cancel = dialogView.findViewById(R.id.btn_request_reserve);
-            dialogBtn_cancel.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    mDialog.show();
-
-                    String name = userName.getText().toString().trim();
-                    String phone = userPhoneNo.getText().toString().trim();
-
-                    if (name.isEmpty()){
-                        Toast.makeText(getActivity(), "Name is empty", Toast.LENGTH_SHORT).show();
-                        mDialog.cancel();
-                        return;
-                    }
-                    if (phone.isEmpty()){
-                        Toast.makeText(getActivity(), "Phone number is empty", Toast.LENGTH_SHORT).show();
-                        mDialog.cancel();
-                        return;
-                    }
-                    if (phone.length() != 10){
-                        Toast.makeText(getActivity(), "Invalid. Please enter a 10 digit number.", Toast.LENGTH_SHORT).show();
-                        mDialog.cancel();
-                        return;
-                    }
-
-                    checkIsProductReservedAndUpdate(activity, name, phone);
-                }
-            });
-
-            AlertDialog alertDialog = builder.create();
-            alertDialog.show();
-        }
-
-        private void checkIsProductReservedAndUpdate(Activity activity, String userName, String userPhoneNo) {
-            FirebaseFirestore.getInstance().collection("Products").document(product.getProductKey()).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-                @Override
-                public void onSuccess(DocumentSnapshot documentSnapshot) {
-                    Product product = documentSnapshot.toObject(Product.class);
-                    if (product != null){
-                        if (!product.getProductReserve()){
-                            Map<String, Object> updateReserveProduct = new HashMap<>();
-                            updateReserveProduct.put("productReserve", true);
-                            updateReserveProduct.put("customerName", userName);
-                            updateReserveProduct.put("customerPhoneNo", userPhoneNo);
-
-                            FirebaseFirestore.getInstance().collection("Products").document(product.getProductKey()).update(updateReserveProduct).addOnSuccessListener(new OnSuccessListener<Void>() {
-                                @Override
-                                public void onSuccess(Void unused) {
-                                    mDialog.cancel();
-                                    activity.finish();
-                                    Toast.makeText(getActivity(), "Reserve request sent successfully", Toast.LENGTH_SHORT).show();
-                                }
-                            });
-                        }
-                        else {
-                            Toast.makeText(getActivity(), "Someone already requested for this product", Toast.LENGTH_LONG).show();
-                            activity.finish();
-                        }
-                    } else {
-                        Toast.makeText(getActivity(), "This product might be removed", Toast.LENGTH_LONG).show();
-                        activity.finish();
-                    }
-                    mDialog.dismiss();
-                }
-            });
-        }
     }
 }
